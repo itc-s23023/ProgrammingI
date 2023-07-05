efd8497b923923d9cb4d14e3efe81c91bd7f7b1a
itc-s23023 <s23023@std.it-college.ac.jp>
2023-07-05 07:31:34 +0000

    add_chap2_7~9

---
+++ CHAPTER02/Q2_7_2.py
@@ -0,0 +1,6 @@
+list = []
+list.append("a")
+list.append("b")
+list.append("c")
+list.append("d")
+print(list[::-1])
---
+++ CHAPTER02/Q2_8_1.py
@@ -0,0 +1,3 @@
+my_list = [1, 2, 3, 4, 5, 6, 7, 8]
+reverse_even = my_list[::-2]
+print(reverse_even)
