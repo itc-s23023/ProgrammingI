3006e9d016f40aedc82b4411f0503e1aa336d8c7
itc-s23023 <s23023@std.it-college.ac.jp>
2023-07-07 00:54:31 +0000

    add_extra

--- CHAPTER02/Q2_5_2_extra.py
+++ CHAPTER02/Q2_5_2_extra.py
@@ -6,9 +6,18 @@
 
 
 
+#len_circle = int(input())
+#for i in range(len_circle):
+#    base_circle = list('●' * len_circle)
+#    base_circle[i] = '○'
+#    result = base_circle
+#    print(" ".join(result))
+
+
+
+
 len_circle = int(input())
 for i in range(len_circle):
-    base_circle = list('●' * len_circle)
-    base_circle[i] = '○'
-    result = base_circle
+    result = ['○' if j == i else '●' for j in range(len_circle)]
     print(" ".join(result))
+
