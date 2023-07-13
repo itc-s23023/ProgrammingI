9a03f0fe3f9c2eaedd74cad996b37c926ff1f87b
itc-s23023 <s23023@std.it-college.ac.jp>
2023-07-13 07:01:11 +0000

    fixed_Q3_6_1.py

--- CHAPTER03/Q3_6_1.py
+++ CHAPTER03/Q3_6_1.py
@@ -3,9 +3,9 @@ import random
 s = str(random.randint(1000, 9999))
 
 while True:
-    user_input = input("4桁の数字を入力")
+    n = input("4桁の数字を入力")
 
-    if user_input == s:
+    if n == s:
         print("一致")
         break
     else:
