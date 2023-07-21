8f5a018247d0b2326a69fe16bd921ff769e20193
itc-s23023 <s23023@std.it-college.ac.jp>
2023-07-21 00:59:07 +0000

    modding_Q4_3_2.py

--- CHAPTER04/Q4_3_2.py
+++ CHAPTER04/Q4_3_2.py
@@ -1,9 +1,27 @@
+# def func_square(*args):
+#     results = []
+#     for n in args:
+#         results.append(n * n)
+#     return results
+
+
+
 def func_square(*args):
     results = []
-    for n in args:
-        results.append(n * n)
-    return results
+    
+    for i in args:
+        results.append(i * i)
+        
+    for i in range(0, len(results), 10):
+        print(results[i:i+10])
 
 
-numbers = [1, 2, 3, 4]
-print(func_square(*numbers))
+
+# def func_square(*args):
+#     results = [i * i for i in args]
+#     for i in range(0, len(results), 10):
+#         print(results[i:i+10])
+
+numbers = list(range(100))
+func_square(*numbers)
+
