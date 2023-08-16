37c80dfbfaeff94bfbdd1af76fa27e2828d761d6
itc-s23023 <s23023@std.it-college.ac.jp>
2023-08-16 05:08:18 +0000

    Q5

---
+++ CHAPTER05/Q5_9_1.py
@@ -0,0 +1,6 @@
+mountain_in_japan = {"fugi": 2276, "kitadake": 3193, "okuhodakadake": 3190, "dummy": 0}
+mountain_in_japan = {"fuji": 3776, "kitadake": 3193, "okuhodakadake": 3190, "dummy": 0}
+mountain_in_japan_sorted = sorted(
+    mountain_in_japan.items(), key=lambda x: x[1], reverse=True
+)
+print(mountain_in_japan_sorted)
